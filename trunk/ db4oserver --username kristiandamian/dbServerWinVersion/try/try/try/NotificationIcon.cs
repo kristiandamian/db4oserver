e2549cdcddbd2981d25254c7238d708716d2a75f
kristiandamian <kristiandamian@44c7cd9d-ed42-0410-b8ec-7d3ffd620026>
2008-01-11 23:49:29 +0000

    Se añadieron las primeras clases para ejecutar tareas, así como la leyenda del GLP a todos los .cs y se añadio la GLP como archivo TXT y se hizo modal las ventanas para que ya no se pierdan.
    AJUA!!

--- trunk/ db4oserver --username kristiandamian/dbServerWinVersion/try/try/try/NotificationIcon.cs
+++ trunk/ db4oserver --username kristiandamian/dbServerWinVersion/try/try/try/NotificationIcon.cs
@@ -1,3 +1,18 @@
+/*  This file is part of db4oserver
+
+    db4oserver is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    db4oserver is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /*
  * Creado por SharpDevelop.
  * Creado por : kristian // Usuario: Administrador
@@ -101,6 +116,7 @@ namespace tryIcon
 
         private void LlamoPantallaPrincipal()
         {
+           
             if (frmprincipal == null || frmprincipal.IsDisposed)
             {
                 frmprincipal = new frmPrincipal();
